b1aefd89f1d7b8c60fd3789859aeed1c2bf882a8
Muhep Atasoy <muhepatasoy06@gmail.com>
2019-08-04 20:08:20 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# youtube-dl API Server
+
+This is a youtube-dl API server! You can make easily youtube-dl integrated apps with this server.
+
+**Currently under development. The server ip and port can be changed in main.py. Will be improved with new updates.**
+
+### Requirements
+
+* Python 3
+* pip
+* FFMPEG
+
+Copy the ffmpeg.exe and ffprobe.exe files to the project directory. Only for Windows. For Linux, copy the following code and paste it into your terminal.
+```
+sudo apt-get install ffmpeg -y
+```
+
+* Install required packages with pip. Copy and paste following code into your console.
+
+```
+pip3 install -r requirements.txt
+```
+
+## Start Server
+
+* Go to project root folder and open console. 
+* Copy and paste following code into your console.
+```
+python main.py
+```
+* Go to http://localhost:1998
+
+## Usage
+
+###### Get Video Info
+```
+http://localhost:1998/api/info?video={video_url}
+```
+
+###### Get Video Info
+```
+http://localhost:1998/api/download?video={video_url}
+```
+It is downloaded to the root directory.
+
+###### Get Download Queue
+```
+http://localhost:1998/api/queue
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ main.py
@@ -0,0 +1,93 @@
+from bottle import route, run, static_file, request
+from youtube_dl import YoutubeDL
+from queue import Queue
+from threading import Thread
+import json
+
+app_defaults = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '256',
+    }],
+}
+
+
+def is_set(variable):
+    try:
+        x = variable
+        return True
+    except KeyError:
+        return False
+
+
+@route('/')
+def index():
+    return static_file('index.html', root='public', mimetype='text/html')
+
+
+@route('/css/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='public/css', mimetype='text/css')
+
+
+@route('/css/<filename:re:.*\.map>')
+def stylesheets_map(filename):
+    return static_file(filename, root='public/css', mimetype='application/octet-stream')
+
+
+@route('/api/<apiname>')
+def api(apiname):
+    try:
+        query = request.query
+        if apiname == 'info':
+            return info(query['video'])
+        elif apiname == 'download':
+            url = query.video
+            yt_queue.put(url)
+            return {"success": True, "message": "Video successfully added to download queue."}
+    except KeyError:
+        return {'status': False, 'message': 'Some parameters is missing!'}
+
+
+def info(video):
+    with YoutubeDL(app_defaults) as ydl:
+        info_dict = ydl.extract_info(video, download=False)
+        return {
+            'status': True,
+            'id': info_dict.get("id", None),
+            'title': info_dict.get('title', None),
+            'description': info_dict.get('description', None),
+            'url': info_dict.get("url", None)
+        }
+
+
+def download(url):
+    with YoutubeDL(app_defaults) as ydl:
+        ydl.download([url])
+
+
+@route('/api/queue', method='GET')
+def q_size():
+    return {"success": True, "size": json.dumps(list(yt_queue.queue))}
+
+
+def work_to_me():
+    while not done:
+        url = yt_queue.get()
+        download(url)
+        yt_queue.task_done()
+
+
+yt_queue = Queue()
+done = False
+thread = Thread(target=work_to_me)
+thread.start()
+
+print("Started download thread")
+
+run(host='localhost', port=1998)
+
+done = True
+thread.join()
